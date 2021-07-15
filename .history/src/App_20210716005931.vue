f73e1a1dc6d190e692dfb9e03272edb0b438f601
Bartosz Pazdur <b.pazdur@onet.pl>
2021-07-16 01:01:59 +0200

    Initial commit
    First commit of project

---
+++ .history/src/App_20210716005931.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'App',
+  setup() {
+    const name = ref('Bartosz')
+
+    return { name }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
